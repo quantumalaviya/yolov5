d464ab26c1e780925678c6822e29a7642c24f89c
quantumalaviya <45961148+quantumalaviya@users.noreply.github.com>
2022-02-01 19:33:54 +0530

    Update augmentations.py

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -265,7 +265,12 @@ def mixup(im, labels, im2, labels2):
     # Applies MixUp augmentation https://arxiv.org/pdf/1710.09412.pdf
     r = np.random.beta(32.0, 32.0)  # mixup ratio, alpha=beta=32.0
     im = (im * r + im2 * (1 - r)).astype(np.uint8)
-    labels = np.concatenate((labels, labels2), 0)
+    if r < 0.2:
+        labels = labels2
+    elif r > 0.8:
+        labels = labels
+    else:
+        labels = np.concatenate((labels, labels2), 0)
     return im, labels
 
 
